b63f3d7db85e5c36dc484fe8037c60cc4b331047
i7source <i7source@yahoo.com>
2024-01-27 08:32:19 -0500

    test backend

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,7 +11,8 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function signup(SignupRequest $request) {
+    public function signup(SignupRequest $request)
+    {
 
         $data = $request->validated();
 
@@ -20,18 +21,14 @@ class AuthController extends Controller
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
-        
-        $token = $user->createToken('main')->plainTextToken;
 
-        // return response([
-        //     'user' => $user,
-        //     'token' => $token,
-        // ]);
+        $token = $user->createToken('main')->plainTextToken;
 
         return response(compact('user', 'token'));
     }
 
-    public function login(LoginRequest $request) {
+    public function login(LoginRequest $request)
+    {
 
         $credentials = $request->validated();
 
@@ -45,14 +42,13 @@ class AuthController extends Controller
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
         return response(compact('user', 'token'));
-
     }
 
-    public function logout(Request $request) {
+    public function logout(Request $request)
+    {
 
         $user = $request->user();
         $user->currentAccessToken()->delete();
         return response('', 204);
-
     }
 }
