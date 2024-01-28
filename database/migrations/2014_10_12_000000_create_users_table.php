d3cfd640e11d7533367c7bd951d984470bfda25a
i7source <i7source@yahoo.com>
2024-01-28 14:41:39 -0500

    add bonus field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('role')->default('user');
             $table->integer('kinah')->default(0);
+            $table->integer('bonus')->default(0);
             $table->integer('coin')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
